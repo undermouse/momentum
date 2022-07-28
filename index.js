5157c22ac64fe7bc00abd119b61377a79be3d54a
plur <plur@live.ru>
2022-07-28 15:29:13 +0300

    add: proper greeting + username in localStorage

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // constants
-const timeEl = document.querySelector('.time');
-const dateEl = document.querySelector('.date');
-const cityEl = document.querySelector('.city');
+const timeEl   = document.querySelector('.time');
+const dateEl   = document.querySelector('.date');
+const cityEl   = document.querySelector('.city');
+const greetEl  = document.querySelector('.greeting');
+const nameEl   = document.querySelector('.name');
+const LS       = window.localStorage;
 
 
 
@@ -23,18 +26,46 @@ const getDate = () => {
   return now.toLocaleString("ru-RU", options);
 }
 
+const setGreeting = () => {
+  let now = new Date();
+  let h = now.getHours();
+  let m = now.getMinutes();
+  let properGreeting = '';
+
+  if (h > 6 && h < 12 && m < 59) {
+    properGreeting = 'Goog morning';
+  } else if (h > 12 && h < 17 && m < 59) {
+    properGreeting = 'Good afternoon';
+  } else if (h > 18 && h < 25 && m < 59) {
+    properGreeting = 'Good evening';
+  } else if (h > 0 && h < 6 && m < 59) {
+    properGreeting = 'Good night';
+  } 
+
+  return properGreeting;
+}
 
 
 
 
-
-
-
-// Invoking
-// Date once on load/reload
-dateEl.textContent = getDate();
 
 // Time every second
 setInterval(() => {
   timeEl.textContent = getTime();
 }, 1000);
+
+// Setting proper time-of-day greeting every minute
+setInterval(() => {
+  greetEl.textContent = setGreeting();
+}, 60000);
+
+// Setiing date() + loading username
+window.addEventListener('load', () => {
+    nameEl.value = LS.getItem('name')
+    dateEl.textContent = getDate();
+})
+
+// Saving username
+nameEl.addEventListener('input', () => {
+  LS.setItem('name', nameEl.value); 
+})
